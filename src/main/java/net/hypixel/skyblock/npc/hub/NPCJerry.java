76db520668a35f05d223142c936ffc539a2800cb
Hamza <122584180+KingGamerXd@users.noreply.github.com>
2024-03-19 12:56:26 +0500

    ..

--- src/main/java/net/hypixel/skyblock/npc/hub/NPCJerry.java
+++ src/main/java/net/hypixel/skyblock/npc/hub/NPCJerry.java
@@ -30,7 +30,6 @@ public class NPCJerry extends SkyblockNPC {
                         "The Skyblock universe is full of islands to explore and resources to discover!",
                         "I will help you start your journey in sandbox.",
                         "Use /ib or free items npc to get starter goods.",
-                        "Use the /hex to upgrade your item at its peak.",
                         "Complete Flor 6 Boss Room.",
                         "Complete All Slayer Bosses.",
                         "New Updates Coming Soon in future!"
